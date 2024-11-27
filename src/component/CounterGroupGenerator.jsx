3ddbdeee2547336d8940fb66e29936a9e3beb4de
HEAL <HEAL@OOCL.COM>
2024-11-27 23:08:25 +0800

    feat: add handleSizeChange & handleReset function in CounterGroupGenerator component

--- src/component/CounterGroupGenerator.jsx
+++ src/component/CounterGroupGenerator.jsx
@@ -1,9 +1,35 @@
 import Counter from "./Counter";
+import {useState} from "react";
 
 const CounterGroupGenerator = () => {
+    const [size, setSize] = useState(0);
+
+    const handleSizeChange = (event) => {
+        console.log("enter handleSizeChange")
+        if (event.target.value >= 0 && event.target.value <= 20) {
+            setSize(event.target.value);
+        }
+    };
+
+    const handleReset = () => {
+        setSize(0);
+    };
+
+
     return (
-        <Counter/>
-    )
+        <div className="input-container">
+            <font>Size:</font>
+            <input
+                min={0}
+                max={20}
+                type="number"
+                value={size}
+                onChange={handleSizeChange}
+            />
+            <button onClick={handleReset}>Reset</button>
+            <Counter/>
+        </div>
+)
 }
 
 export default CounterGroupGenerator;
