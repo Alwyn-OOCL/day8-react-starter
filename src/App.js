6441fd3edd9a43837b91089521833b794f0fd627
HEAL <HEAL@OOCL.COM>
2024-11-27 22:59:34 +0800

    feat: initial CounterGroupGenerator component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./component/Counter";
+import CounterGroupGenerator from "./component/CounterGroupGenerator";
 
 function App() {
   return (
-    <Counter/>
+    <CounterGroupGenerator/>
   );
 }
 
---
+++ src/component/CounterGroupGenerator.jsx
@@ -0,0 +1,9 @@
+import Counter from "./Counter";
+
+const CounterGroupGenerator = () => {
+    return (
+        <Counter/>
+    )
+}
+
+export default CounterGroupGenerator;
